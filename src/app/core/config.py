488135d2a2ae5c66dced96474e23539bfeb7a62a
Hovhannes <126023074+Hovo-Dev@users.noreply.github.com>
2026-05-18 10:25:01 +0400

    Initialize staging

---
+++ src/app/core/config.py
@@ -0,0 +1,236 @@
+import logging
+from enum import Enum
+from typing import Self
+
+from pydantic import SecretStr, computed_field, field_validator, model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+logger = logging.getLogger(__name__)
+
+
+class AppSettings(BaseSettings):
+    APP_NAME: str = "FastAPI app"
+    APP_DESCRIPTION: str | None = None
+    APP_VERSION: str | None = None
+    APP_BACKEND_HOST: str = "http://localhost:8000"
+    APP_FRONTEND_HOST: str | None = None
+    LICENSE_NAME: str | None = None
+    CONTACT_NAME: str | None = None
+    CONTACT_EMAIL: str | None = None
+
+    @field_validator("APP_BACKEND_HOST", "APP_FRONTEND_HOST", mode="after")
+    @classmethod
+    def validate_hosts(cls, host: str) -> str:
+        if host is not None and not (host.startswith("http://") or host.startswith("https://")):
+            raise ValueError(
+                f"HOSTS must define their protocol and start with http:// or https://. Received the host '{host}'."
+            )
+        return host
+
+
+class LogLevelOption(str, Enum):
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+
+
+class LoggingSettings(BaseSettings):
+    LOG_LEVEL: LogLevelOption = LogLevelOption.INFO
+    LOG_FORMAT_AS_JSON: bool = False
+    LOG_TO_FILE: bool = False
+
+
+class CryptSettings(BaseSettings):
+    SECRET_KEY: SecretStr = SecretStr("secret-key")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
+
+class DatabaseSettings(BaseSettings):
+    pass
+
+
+class PostgresSettings(DatabaseSettings):
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str = "postgres"
+    POSTGRES_ASYNC_PREFIX: str = "postgresql+asyncpg://"
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def POSTGRES_URI(self) -> str:
+        credentials = f"{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+        location = f"{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return f"{credentials}@{location}"
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def POSTGRES_URL(self) -> str:
+        return f"{self.POSTGRES_ASYNC_PREFIX}{self.POSTGRES_URI}"
+
+
+class FirstUserSettings(BaseSettings):
+    ADMIN_NAME: str = "admin"
+    ADMIN_EMAIL: str = "admin@admin.com"
+    ADMIN_USERNAME: str = "admin"
+    ADMIN_PASSWORD: str = "!Ch4ng3Th1sP4ssW0rd!"
+
+
+class TestSettings(BaseSettings):
+    ...
+
+
+class RedisCacheSettings(BaseSettings):
+    REDIS_CACHE_HOST: str = "localhost"
+    REDIS_CACHE_PORT: int = 6379
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def REDIS_CACHE_URL(self) -> str:
+        return f"redis://{self.REDIS_CACHE_HOST}:{self.REDIS_CACHE_PORT}"
+
+
+class ClientSideCacheSettings(BaseSettings):
+    CLIENT_CACHE_MAX_AGE: int = 60
+
+
+class RedisQueueSettings(BaseSettings):
+    REDIS_QUEUE_HOST: str = "localhost"
+    REDIS_QUEUE_PORT: int = 6379
+
+
+class RedisRateLimiterSettings(BaseSettings):
+    REDIS_RATE_LIMIT_HOST: str = "localhost"
+    REDIS_RATE_LIMIT_PORT: int = 6379
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def REDIS_RATE_LIMIT_URL(self) -> str:
+        return f"redis://{self.REDIS_RATE_LIMIT_HOST}:{self.REDIS_RATE_LIMIT_PORT}"
+
+
+class DefaultRateLimitSettings(BaseSettings):
+    DEFAULT_RATE_LIMIT_LIMIT: int = 10
+    DEFAULT_RATE_LIMIT_PERIOD: int = 3600
+
+
+class CRUDAdminSettings(BaseSettings):
+    CRUD_ADMIN_ENABLED: bool = True
+    CRUD_ADMIN_MOUNT_PATH: str = "/admin"
+
+    CRUD_ADMIN_ALLOWED_IPS_LIST: list[str] | None = None
+    CRUD_ADMIN_ALLOWED_NETWORKS_LIST: list[str] | None = None
+    CRUD_ADMIN_MAX_SESSIONS: int = 10
+    CRUD_ADMIN_SESSION_TIMEOUT: int = 1440
+    SESSION_SECURE_COOKIES: bool = True
+
+    CRUD_ADMIN_TRACK_EVENTS: bool = True
+    CRUD_ADMIN_TRACK_SESSIONS: bool = True
+
+    CRUD_ADMIN_REDIS_ENABLED: bool = False
+    CRUD_ADMIN_REDIS_HOST: str = "localhost"
+    CRUD_ADMIN_REDIS_PORT: int = 6379
+    CRUD_ADMIN_REDIS_DB: int = 0
+    CRUD_ADMIN_REDIS_PASSWORD: str | None = "None"
+    CRUD_ADMIN_REDIS_SSL: bool = False
+
+
+class EnvironmentOption(str, Enum):
+    LOCAL = "LOCAL"
+    STAGING = "STAGING"
+    PRODUCTION = "PRODUCTION"
+
+
+class EnvironmentSettings(BaseSettings):
+    ENVIRONMENT: EnvironmentOption = EnvironmentOption.LOCAL
+
+    @field_validator("ENVIRONMENT", mode="before")
+    @classmethod
+    def normalize_environment(cls, v: str) -> str:
+        return v.upper()
+
+
+class CORSSettings(BaseSettings):
+    CORS_ORIGINS: list[str] = ["*"]
+    CORS_METHODS: list[str] = ["*"]
+    CORS_HEADERS: list[str] = ["*"]
+
+
+class AuthSettings(BaseSettings):
+    ENABLE_PASSWORD_AUTH: bool = True
+    GOOGLE_CLIENT_ID: str | None = None
+    GOOGLE_CLIENT_SECRET: str | None = None
+    MICROSOFT_CLIENT_ID: str | None = None
+    MICROSOFT_CLIENT_SECRET: str | None = None
+    MICROSOFT_TENANT: str | None = None
+    GITHUB_CLIENT_ID: str | None = None
+    GITHUB_CLIENT_SECRET: str | None = None
+
+
+class Settings(
+    AppSettings,
+    PostgresSettings,
+    CryptSettings,
+    FirstUserSettings,
+    TestSettings,
+    RedisCacheSettings,
+    ClientSideCacheSettings,
+    RedisQueueSettings,
+    RedisRateLimiterSettings,
+    DefaultRateLimitSettings,
+    CRUDAdminSettings,
+    EnvironmentSettings,
+    CORSSettings,
+    LoggingSettings,
+    AuthSettings,
+):
+    model_config = SettingsConfigDict(
+        case_sensitive=True,
+        extra="ignore",
+    )
+
+    @model_validator(mode="after")
+    def validate_environment_settings(self) -> Self:
+        """The validation should not modify any of the settings.
+
+        It should provide feedback to the user if any misconfiguration is detected, or raise and error if the
+        misconfiguration is critical.
+        """
+        environment = self.ENVIRONMENT.value
+        if self.ENVIRONMENT == EnvironmentOption.LOCAL:
+            pass
+        elif self.ENVIRONMENT == EnvironmentOption.STAGING:
+            if "*" in self.CORS_ORIGINS:
+                logger.warning(
+                    f"For security, in a {environment} environment CORS_ORIGINS should not include '*'. "
+                    "It is recommended to specify explicit origins (e.g., ['https://staging.example.com'])."
+                )
+        elif self.ENVIRONMENT == EnvironmentOption.PRODUCTION:
+            if "*" in self.CORS_ORIGINS:
+                raise ValueError(
+                    f"For security, in a {environment} environment CORS_ORIGINS cannot include '*'. "
+                    "You must specify explicit allowed origins (e.g., ['https://example.com', 'https://www.example.com'])."
+                )
+            if self.APP_FRONTEND_HOST and not self.APP_FRONTEND_HOST.startswith("https://"):
+                raise ValueError(
+                    f"In {environment} environment, APP_FRONTEND_HOST must start with the https:// protocol. "
+                    f"Received the host '{self.APP_FRONTEND_HOST}'."
+                )
+            if self.LOG_LEVEL == LogLevelOption.DEBUG:
+                logger.warning(
+                    f"In a {environment} environment, it is recommended to set LOG_LEVEL to INFO, WARNING, or ERROR. "
+                    "It is currently being set to DEBUG."
+                )
+            if self.LOG_FORMAT_AS_JSON is False:
+                logger.warning(
+                    f"In a {environment} environment, it is recommended to set LOG_FORMAT_AS_JSON to true "
+                    "if you are using log aggregation tools."
+                )
+        return self
+
+
+settings = Settings()
